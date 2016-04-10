c3507cf965a8e11330e427f91c8b74fe0e4a838d
Jase <unmric@gmail.com>
2016-04-11 02:27:25 +0800

    ocd

--- browser.js
+++ browser.js
@@ -5,9 +5,9 @@ const BrowserWindow = require('browser-window')
 
 app.once('ready', () => {
   const mainWindow = new BrowserWindow({
-    title: 'Seals',
-    width: 800,
-    height: 600
+    title: 'Seals'
+    , width: 800
+    , height: 600
   })
 
   mainWindow.setMenuBarVisibility(false)
